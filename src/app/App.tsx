3a0df5d36f99f14657e69ad4e96c4284e33b286e
Premier <sayed3la2@gmail.com>
2026-09-02 22:28:40 +0300

    feat: surface financial visibility controls in super admin

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
 import { AppProviders } from './providers';
 import { AppRoutes } from './routes';
+import { FinancialVisibilityAdminControl } from '@/features/admin/components/FinancialVisibilityAdminControl';
 
 export default function App() {
   return (
     <AppProviders>
       <AppRoutes />
+      <FinancialVisibilityAdminControl />
     </AppProviders>
   );
 }
